bf3fb9f198524a765a8ec49e3ac2881c786e729e
Joel McCune <knu2xs@gmail.com>
2022-04-10 08:18:38 -0700

    various fixes and geometry extraction

--- src/river_reacher/main.py
+++ src/river_reacher/main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Iterable, List, Optional, Union
 from warnings import warn
 
+from arcgis.geometry import Geometry, Polyline
 from requests import get
 
 
@@ -36,6 +37,9 @@ class Reach(object):
         with open(path) as f:
             reach._aw_dict = json.load(f)
 
+        # set the id
+        reach.aw_id = reach._aw_dict['CContainerViewJSON_view']['CRiverMainGadgetJSON_main']['info']['id']
+
         return reach
 
     @property
@@ -45,7 +49,7 @@ class Reach(object):
         """
         return self._aw_id
 
-    @property.setter
+    @aw_id.setter
     def aw_id(self, in_id=Union[int, str]) -> str:
         # make sure is formatted as a string
         if isinstance(in_id, int):
@@ -65,45 +69,45 @@ class Reach(object):
             url = None
         return url
     
-
-    @lru_cache
     @property
+    @lru_cache
     def aw_dict(self) -> dict:
         """
         Retrieve (if necessary) and return the dictionary of raw reach data from American Whitewater.
         """
-        # create the string to retrieve the data from
-        url = f"https://www.americanwhitewater.org/content/River/detail/id/{self.aw_id}/.json"
+        # if not set, retrieve from AW
+        if self._aw_dict is None:
 
-        # retrieve the data from AW - retrying to handle dropped requests
-        attempts = 0
-        status_code = 0
+            # create the string to retrieve the data from
+            url = f"https://www.americanwhitewater.org/content/River/detail/id/{self.aw_id}/.json"
 
-        while attempts < 10 and status_code != 200:
-            res = get(url)
-            if res.status_code == 200 and len(res.content):
-                aw_json = res.json()
-            elif res.status_code == 200 and not len(res.content):
-                aw_json = None
-            elif res.status_code == 500:
-                aw_json = None
-            else:
-                attempts += 1
+            # retrieve the data from AW - retrying to handle dropped requests
+            attempts = 0
+            status_code = 0
 
-        # if cannot retrieve any data, let somebody know
-        if aw_json is None:
-            warn(f"Cannot download data for reach_id {self.aw_id}")
+            while attempts < 10 and status_code != 200:
+                res = get(url)
+                if res.status_code == 200 and len(res.content):
+                    aw_json = res.json()
+                elif res.status_code == 200 and not len(res.content):
+                    aw_json = None
+                elif res.status_code == 500:
+                    aw_json = None
+                else:
+                    attempts += 1
+
+            # if cannot retrieve any data, let somebody know
+            if aw_json is None:
+                warn(f"Cannot download data for reach_id {self.aw_id}")
+
+        # if set, just return
+        else:
+            aw_json = self._aw_dict
 
         return aw_json
 
 
-    @property.setter
-    def aw_dict(self, input_dictionary) -> dict:
-        self.aw_dict = input_dictionary
-        return self.aw_dict
-
-
-    def get_aw_property(self, key_and_index_list=List[str, int]) -> Union[str, dict]:
+    def get_aw_property(self, key_and_index_list=List[Union[str, int]]) -> Union[str, dict]:
         """
         Retrieve deep nested properties from within the American Whitewater JSON.
 
@@ -117,7 +121,7 @@ class Reach(object):
         assert self._aw_id is not None, "Must define an AW ID to retrieve properties."
 
         # if the key and index list is just a single value, convert to list
-        if not isinstance(key_and_index_list, Iterable):
+        if isinstance(key_and_index_list, (str, int)):
             key_and_index_list = list(key_and_index_list)
 
         # initialize the return value to the entire AW dict
@@ -128,3 +132,16 @@ class Reach(object):
             ret_val = ret_val[key_idx]
 
         return ret_val
+
+    @property
+    def geometry(self) -> Polyline:
+        """
+        Line geometry of the reach.
+        """
+        # extract the coordinates from the AW JSON
+        coord_list = self.get_aw_property(['CContainerViewJSON_view', 'CRiverMainGadgetJSON_main', 'info', 'geom', 'coordinates'])
+
+        # convert the coordinates to Esri JSON
+        geom = Polyline({"paths" : [coord_list],"spatialReference" : {"wkid" : 4326}})
+
+        return geom
